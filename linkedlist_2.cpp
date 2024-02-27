80c2e96a37cfe9d8df99539d36053c7a068d9c1b
student-Dharmendra <dk5747420@gmail.com>
2024-02-27 23:07:37 +0530

    initial commit

---
+++ linkedlist_2.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node *Next;
+    Node(int value)
+    {
+        data=value;
+        Next=NULL;
+    }
+};
+int main()
+{
+    Node *head=NULL;
+    Node *tail=NULL;
+    int arr[]={2,4,6,8,10};
+    for(int i=0;i<5;i++)
+    {
+        if(head==NULL)
+        {
+            head=new Node(arr[i]);
+            tail=head;
+        }
+        else
+        {
+            tail->Next=new Node(arr[i]);
+            tail=tail->Next;
+        }
+
+    }
+    Node *temp;
+    temp=head;
+    while(temp)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->Next;
+    }
+}
